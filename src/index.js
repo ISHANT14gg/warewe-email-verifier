22159574a6a3e151170cdde2b46387c41651c04c
jexo <sharmaishant9418556349@gmail.com>
2026-03-26 22:51:20 +0530

    fix: Auto-start server when index.js is executed directly (Render fix)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const { verifyEmail } = require('./verifyEmail');
-const { getDidYouMean } = require('./getDidYouMean');
-
-module.exports = { verifyEmail, getDidYouMean };
+if (require.main === module) {
+  // If run directly (e.g. `node src/index.js`), start the server
+  require('./server');
+} else {
+  // If required as a library, export the functions
+  const { verifyEmail } = require('./verifyEmail');
+  const { getDidYouMean } = require('./getDidYouMean');
+  module.exports = { verifyEmail, getDidYouMean };
+}
